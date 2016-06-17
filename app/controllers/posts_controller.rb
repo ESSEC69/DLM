c42e787c405b1be49de515f24b44698f9e78c378
Cyrille Ducret <cyrille.cpi@gmail.com>
2016-06-17 13:29:11 -0500

    focus on articles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,18 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, only: [ :show ]
+
   def index
     @posts = Post.all
   end
 
   def new
     @post = Post.new
+  end
+
+  def show
+
+
   end
 
   def edit
@@ -39,7 +46,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :body, :description)
+  end
 
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 
