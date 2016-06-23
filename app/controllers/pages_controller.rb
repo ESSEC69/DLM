bb75bc5932e67c5f69402718eca113a733d4a40f
Cyrille Ducret <cyrille.cpi@gmail.com>
2016-06-23 18:43:22 -0500

    front

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def home
-    @posts = Post.last(3).reverse
+    @posts = Post.last(6).reverse
+    @posts1=@posts.first(3)
+    @posts2=@posts.last(3)
 
   end
 end
